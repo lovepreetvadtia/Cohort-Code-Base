c9a1808e5a87f1cce2a4d6fa2e11e47d750665d7
Lovepreet <lovevadtia2938@gmail.com>
2025-10-29 05:02:03 +0530

    Updated At 29 Oct

---
+++ Day 28-Data Types In Js/script.js
@@ -0,0 +1,21 @@
+// Write one line in your own words: “If HTML is skeleton, CSS is clothes — what is JS?”
+// If HTML is skeleton and CSS is clothes then we can imagine js as our nervous system.
+
+// Write one example of where you’ve seen JS being used (like popup, animation, etc.)
+// The Example of js which i saw on sheryians Website when i click on "Request A Callback" button then its shows me a popup
+
+// Declare your name using all three: var a = “Harsh”; let b = “Sheryians”; const c= “School”
+var a= "Lovepreet"
+let b= "Singh"
+const c = "Vadtia";
+
+
+//Try Reassign them:-  a = “Updated”; b = “Updated”; c = “Updated”
+
+let a = "Updated"
+
+a = "not updated";
+
+// With using let we reassign but cant redeclare 
+
+var a "Updated";
