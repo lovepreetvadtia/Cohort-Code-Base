507797a3e5505488644eebbad88d664d3c536fca
Lovepreet Singh <lovepreetvadtia@gmail.com>
2026-01-31 07:09:37 +0530

    Day 90 Update

--- Day-90-DB-MainClass/server.js
+++ Day-90-DB-MainClass/server.js
@@ -1,5 +1,7 @@
 const app = require('./src/app')
 const ConnectToDB = require('./config/database') 
+
+require('dotenv').config()
 ConnectToDB()
 app.listen(3000,()=>{
     console.log('App Is Running On Port 3000')
