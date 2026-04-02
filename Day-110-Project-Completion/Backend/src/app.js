2c66c0039ff078778f1acf5bcc5e1ceae7761845
Lovepreet Singh <lovepreetvadtia@gmail.com>
2026-04-02 06:17:11 +0530

    Change

--- Day-110-Project-Completion/Backend/src/app.js
+++ Day-110-Project-Completion/Backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "https://full-stack-app-y3qs.onrender.com/"
 }))
 
 // Routes
